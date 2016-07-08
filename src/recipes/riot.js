4f8f87585ffa515d0d507536c88a46acbb708f6a
Fumio Furukawa <fumio@jumilla.me>
2016-07-08 18:58:13 +0900

    Add riot recipe.

---
+++ src/recipes/riot.js
@@ -0,0 +1,49 @@
+
+import util from '../util'
+
+/**
+ * parameters
+ *     .inputs : array
+ *     .input  : string
+ *     .output : string
+ *     .cleans : array
+ *     .clean  : string
+ */
+module.exports = function($, builder, parameters = {}) {
+	let config = $.config
+	let inputPaths = parameters.inputs || (parameters.input ? [parameters.input] : [])
+	let outputDirectory = parameters.output
+	let cleanPaths = parameters.cleans || (parameters.clean ? [parameters.clean] : [])
+	let taskConfig = Object.assign(config.stylus, parameters.config || {})
+
+	$.gulp.task(builder.task, builder.dependentTasks, () => {
+		if (!util.isPluginInstalled('riot', 'gulp-riot')) return
+		if (!util.isValidGlobs(inputPaths)) return
+
+		return $.gulp.src(inputPaths)
+			.pipe($.riot(taskConfig)
+				.on('error', err => {
+					$.notify.onError({
+						title: 'Gulp compile failed',
+						message: '<%= error.message %>',
+						onLast: true,
+					})(err)
+
+					this.emit('end')
+				})
+			)
+			.pipe($.notify({
+				title: 'Gulp compile success!',
+				message: '<%= file.relative %>',
+			}))
+			.pipe($.if(config.sourcemaps, $.sourcemaps.init({ loadMaps: true })))
+			.pipe($.if(config.production, $.uglify(config.js.uglify)))
+			.pipe($.if(config.sourcemaps, $.sourcemaps.write('.')))
+			.pipe($.gulp.dest(outputDirectory))
+			.on('end', () => {
+				$.del.sync(cleanPaths)
+			})
+
+		return result
+	})
+}
